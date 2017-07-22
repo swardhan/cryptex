02e047906d3bbf116ef5261e9e45037aeb9132e2
saumya <saumyawardhan1@gmail.com>
2017-07-22 17:15:38 +0530

    Add Rank

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def rank user
+    User.all.order('level desc, submit_time desc').map(&:id).index(user.id)+1
+  end
 end
