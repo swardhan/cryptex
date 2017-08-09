623241054cd694f540e9635b54445c5715c68638
saumya <saumyawardhan1@gmail.com>
2017-08-09 22:29:04 +0530

    Change Views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,37 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  attr_accessor :login
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  def rank user
-    User.all.order('level desc, submit_time desc').map(&:id).index(user.id)+1
+
+  validates :username,
+         :presence => true,
+         :uniqueness => {
+           :case_sensitive => false
+         }
+
+  validate :validate_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
+
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+        where(conditions.to_h).first
+    end
+  end
+
+  def rank
+    User.all.order('level desc, submit_time desc').map(&:id).index(self.id)+1
   end
 end
